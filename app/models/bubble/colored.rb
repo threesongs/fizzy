dd08991104484fcfd27ef79f6053ad8f7d3fe66e
Jason Zimdars <jz@37signals.com>
2025-01-27 17:18:59 -0600

    Add a new, more cheerful default color

--- app/models/bubble/colored.rb
+++ app/models/bubble/colored.rb
@@ -1,9 +1,9 @@
 module Bubble::Colored
   extend ActiveSupport::Concern
 
-  COLORS = %w[ #BF1B1B #ED3F1C #ED8008 #7C956B #698F9C #3B4B59 #5D618F #3B3633 #67695E ]
+  COLORS = %w[ #BF1B1B #ED3F1C #ED8008 #7C956B #698F9C #266ec3 #3B4B59 #5D618F #3B3633 #67695E ]
 
   included do
-    attribute :color, default: "#698F9C"
+    attribute :color, default: "#266ec3"
   end
 end
